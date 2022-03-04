b1b1795fc8829e5245a662cddbd3ee67fb661e67
airplane9876 <tjdnf2eoeld@naver.com>
2022-03-04 16:32:14 +0900

    init commit

---
+++ deploy-commands.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require("dotenv").config();
+
+const commands = [
+	new SlashCommandBuilder().setName('map').setDescription('지금 일겜 맵 머임?'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
---
+++ index.js
@@ -0,0 +1,73 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const axios = require('axios');
+const cheerio = require('cheerio');
+require("dotenv").config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+const getHtml = async () => {
+    try {
+        return await axios.get('https://apexlegendsstatus.com/current-map');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getHtml()
+  .then((html) => {
+    // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
+    // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
+    const $ = cheerio.load(html.data);
+    const data = {
+      mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h1:nth-child(1)').text(),
+    };
+    return data;
+  })
+  .then((res) => console.log(res));
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'map') {
+        try {
+            const test = await getHtml()
+            .then((html) => {
+                // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
+                // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
+                const $ = cheerio.load(html.data);
+                const map = {
+                    mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h1:nth-child(1)').text(),
+                };
+                const time = {
+                    mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h5:nth-child(4)').text(),
+                };
+                return { map, time };
+            })
+            console.log(test.time.mainContents.split(' ')[3])
+            const hour = (parseInt(test.time.mainContents.split(' ')[3].split(':')[0]) + 9) % 24;
+            const min = test.time.mainContents.split(' ')[3].split(':')[1];
+            console.log(hour, min)
+            await interaction.reply(`일겜 맵 : ${test.map.mainContents}\n종료시간 : ${hour}시 ${min}분`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+		
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
