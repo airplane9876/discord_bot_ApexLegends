619684d8a634e43a8eb3a8f7d328a1ef7580f5a9
airplane9876 <tjdnf2eoeld@naver.com>
2022-03-13 12:39:32 +0900

    2022. 03. 13. - 일겜 맵 로테이션 3개까지 출력하는 명령어 추가

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,13 +5,17 @@ require("dotenv").config();
 
 const commands = [
 	new SlashCommandBuilder().setName('map').setDescription('지금 일겜 맵 머임?'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('maplist').setDescription('제발 올림푸스'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
+// rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands in test'))
+// 	.catch(console.error);
+
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log('Successfully registered application commands in oreore'))
 	.catch(console.error);
--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
-const getHtml = async () => {
+const getHtmlForMap = async () => {
     try {
         return await axios.get('https://apexlegendsstatus.com/current-map');
     } catch (error) {
@@ -20,38 +20,35 @@ const getHtml = async () => {
     }
 };
 
-getHtml()
-  .then((html) => {
-    // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
-    // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
-    const $ = cheerio.load(html.data);
-    const data = {
-      mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h1:nth-child(1)').text(),
-    };
-    return data;
-  })
-  .then((res) => console.log(res));
+const getHtmlForMapList = async () => {
+    try {
+        return await axios.get('https://apexlegendsstatus.com/current-map/battle_royale/pubs');
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+    const { commandName } = interaction;
 
-	if (commandName === 'map') {
+    // 현재 맵
+    if (commandName === 'map') {
         try {
-            const test = await getHtml()
-            .then((html) => {
-                // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
-                // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
-                const $ = cheerio.load(html.data);
-                const map = {
-                    mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h1:nth-child(1)').text(),
-                };
-                const time = {
-                    mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h5:nth-child(4)').text(),
-                };
-                return { map, time };
-            })
+            const test = await getHtmlForMap()
+                .then((html) => {
+                    // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
+                    // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
+                    const $ = cheerio.load(html.data);
+                    const map = {
+                        mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h1:nth-child(1)').text(),
+                    };
+                    const time = {
+                        mainContents: $('body > main > div > div:nth-child(3) > div:nth-child(1) > div.col-lg-8.olympus > div > h5:nth-child(4)').text(),
+                    };
+                    return { map, time };
+                })
             console.log(test.time.mainContents.split(' ')[3])
             const hour = (parseInt(test.time.mainContents.split(' ')[3].split(':')[0]) + 9) % 24;
             const min = test.time.mainContents.split(' ')[3].split(':')[1];
@@ -61,12 +58,38 @@ client.on('interactionCreate', async interaction => {
         catch (err) {
             console.log(err)
         }
-		
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
+    }
+    // 맵 세개까지
+    else if (commandName === 'maplist') {
+        try {
+            const test = await getHtmlForMapList()
+                .then((html) => {
+                    // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
+                    // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
+                    const $ = cheerio.load(html.data);
+                    const map = [];
+                    const time = [];
+                    for (let i = 3; i < 6; i++) {
+                        map.push($(`body > main > div > div:nth-child(${i}) > h3`).text())
+                        time.push($(`body > main > div > div:nth-child(${i}) > p`).text())
+                    }
+                    return { map, time };
+                })
+            let reply = '';
+            for (let i = 0; i < 3; i++) {
+                const hour = (parseInt(test.time[i].split(' ')[4].split(':')[0]) + 9) % 24;
+                const min = test.time[i].split(' ')[4].split(':')[1];
+                console.log(hour, min)
+                reply += `맵 : ${test.map[i]}\t종료시간 : ${hour}시 ${min}분\n`
+            }
+            await interaction.reply(reply);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
 });
 
 // Login to Discord with your client's token
