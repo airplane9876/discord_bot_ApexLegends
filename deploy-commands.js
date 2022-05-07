1a731bfb128317ba8f675df581e901bbce9ed670
airplane9876 <tjdnf2eoeld@naver.com>
2022-05-07 16:25:25 +0900

    2022. 05. 07. - deploy-commands oreore로 최신화

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,10 +12,10 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands in test'))
-	.catch(console.error);
-
-// rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-// 	.then(() => console.log('Successfully registered application commands in oreore'))
+// rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TEST_GUILD_ID), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands in test'))
 // 	.catch(console.error);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands in oreore'))
+	.catch(console.error);
